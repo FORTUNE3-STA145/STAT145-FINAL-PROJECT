c83941811587023357ed790dc92e23f17810ccec
FORTUNE3-STA145 <fortune3@tcnj.edu>
2025-11-24 15:14:28 -0500

    Create STA145-FORTUNE3.R

---
+++ STA145-FORTUNE3.R
@@ -0,0 +1,36 @@
+## Project:  STA 145, Fall 2025, Final Project
+# Located:   TCNJ Google Drive
+# File Name: template
+# Date:      2025_11_23
+# Who:       Elise Fortuna
+
+## Load packages
+# NOTES: Install packages if these commands return an error!
+library(readr)
+library(dplyr)
+library(tidytext)
+library(ggplot2)
+library(haven)
+library(forcats)
+library(psych)
+
+# set working directory
+setwd("/courses/STA145/fortune3")
+
+
+# Load data
+data <-read_delim("data.csv")
+
+describe("data")
+
+data_complete <- data %>%
+  filter(complete.cases(.))
+
+#####################Table 1: DESCRIPTIVE STATISTICS#########################################
+# Governor salary
+table(data$gov_salary)
+
+# Governor educated
+table(data$gov_education
+
+####################Figure 1: Contigency Table###############################################
